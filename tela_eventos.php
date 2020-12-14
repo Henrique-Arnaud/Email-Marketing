1b6b6dae47af0452804da80833885e12452dd8ef
Henrique <henriquearnaud@live.com>
2020-12-14 01:36:01 -0300

    sistema de edição iniciado

--- tela_eventos.php
+++ tela_eventos.php
@@ -26,21 +26,112 @@ if (!securePage($_SERVER['PHP_SELF'])) {
 
 <?php
 
-$evento = $_POST['evento'];
-
-$contatos = $db->query("SELECT nome FROM contato WHERE evento LIKE ?", [$evento]);
+if (!empty($_POST['evento'])) {
+	$evento = $_POST['evento'];
+	$contatos = $db->query("SELECT contato.id, contato.nome as contato_nome, eventos.nome as evento_nome, contato.email, eventos.empresa FROM contato inner join eventos on contato.id_evento = eventos.id WHERE eventos.id = ?", [$evento])->results();
+}
+$lista_eventos = $db->query("SELECT * from eventos")->results();
+//primeiro crio um contador para ver em qual loop o foreach chegou
+$cont = 0;
+//aqui recebo na variavel selecionado o valor de evento (que é o id, consequentemente a posicao na listinha) caso já tenha rolado um sibmit 
+if (!empty($_POST['evento'])) {
+	$selecionado = $evento;
+}
+//caso nao tenha tido submit, ele define como 1
+else {
+	$selecionado = 1;
+}
+if(!empty($_POST['btn_read'])){
+	foreach($lista_eventos as $linha_event){
+		if($linha_event->nome == $evento_nome){
+			$selecionado = $linha_event->id;
+		}
+	}
+}
 
 ?>
+<!--<style>
+
+	table, th, tr, td{
+		margin: 1%;
+		border: 1px solid;
+	}
+
+</style>-->
 
 <div class="row">
 	<div class="col-sm-12">
-		<form method="POST">
-			<label for="evento">Evento</label>
-			<input type="text" id="evento" name="evento"><br>
+		<form method="post">
+			<h3></label>Selecione o Evento desejado:</h3>
 
-			<label for="nome">Participantes</label>
-			<p><?php print_r($contatos) ?></P>
-			<input type="submit">
+			<select name="evento" class="custom-select custom-select-lg mb-3">
+				<?php foreach ($lista_eventos as $linha_evento) {
+					//aqui eu faço o contador rolar e ver em qual verificação do foreach ele está
+					$cont++; ?>
+					<!--Aqui nesse if eu vejo se o selecionado corresponde ao elemento lido no foreach, caso true eu deixo ele Selecionado -->
+					<option <?php if ($selecionado == $cont) {
+								echo 'Selected';
+							} ?> value="<?php echo $linha_evento->id ?>"><?php echo $linha_evento->nome ?></option>
+				<?php } ?>
+			</select>
+
+			<input type="submit" value="Selecionar Dados" class="btn btn-dark">
+
+		</form>
+		<form method="post">
+			<table style="margin-top: 5%;" class="table">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">
+							ID
+						</th>
+						<th scope="col">
+							Nome
+						</th>
+						<th scope="col">
+							Email
+						</th>
+						<th scope="col">
+							Evento
+						</th>
+						<th scope="col">
+							Empresa
+						</th>
+						<th scope="col">
+							
+						</th>
+					</tr>
+				</thead>
+				<?php
+				if (!empty($_POST['evento'])) {
+					foreach ($contatos as $contato) { ?>
+						<tr>
+							<td>
+								<?php echo $contato->id ?>
+							</td>
+							<td>
+								<input type="text" name="contato_nome" class="form-control-plaintext" value="<?php echo $contato->contato_nome ?>">
+							</td>
+							<td>
+								<input type="text" name="email" class="form-control-plaintext" value="<?php echo $contato->email ?>">
+							</td>
+							<td>
+								<input type="text" readonly name="evento_nome" class="form-control-plaintext" value="<?php echo $contato->evento_nome ?>">
+							</td>
+							<td>
+								<input type="text" readonly class="form-control-plaintext" value="<?php echo $contato->empresa ?>">
+							</td>
+							<td>	
+								<span class="fa fa-trash btn btn-dark"></span>
+							</td>
+						</tr>
+				<?php }
+				}
+				?>
+			</table>
+			<div>
+				<input readonly <?php if(!empty($_POST['evento'])) echo 'type="submit"' ?> name="btn_edit" value="Editar Dados" class="btn btn-dark">
+			</div>
 		</form>
 	</div>
 </div>
--- tela_upload_lista.php
+++ tela_upload_lista.php
@@ -18,24 +18,68 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 require_once 'users/init.php';
-require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
+require_once $abs_us_root . $us_url_root . 'users/includes/template/prep.php';
 if (!securePage($_SERVER['PHP_SELF'])) {
     die();
 }
 ?>
 
 <?php
- 
+
+if (!empty($_FILES['lista'])) {
+    $nome_temporario = $_FILES['lista']['tmp_name'];
+
+    $nome_real = $_FILES['lista']['name'];
+
+    if(move_uploaded_file($nome_temporario, $nome_real)){
+        echo '<div class="alert alert-success" role="alert">
+        Lista enviada com sucesso!
+      </div>';
+    }
+    else{
+        echo '<div class="alert alert-danger" role="alert">
+        Lista não enviada!
+      </div>';
+    }
+
+    $arquivo = fopen($nome_real, "r");
+
+    $header = fgetcsv($arquivo, 1000, ";");
+
+    while ($row = fgetcsv($arquivo, 1000, ";")) {
+
+        $usuarios[] = array_combine($header, $row);
+    }
+
+    foreach ($usuarios as $usuario) {
+        if (filter_var($usuario['nome'], FILTER_VALIDATE_EMAIL)) {
+            $aux = $usuario['nome'];
+            $usuario['nome'] = $usuario['email'];
+            $usuario['email'] = $aux;
+        }
+        $db->insert('contato', $usuario);
+    }
+
+    fclose($arquivo);
+
+}
 ?>
 
 <div class="row">
-	<div class="col-sm-12">
-        <form action="confirmacao_upload.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="lista">
-        <input type="submit" value="Fazer Upload">
+    <div class="col">
+        <h2 class="custom-form">Insira uma lista que contenha os dados dos contatos: </h2>
+        <form style="margin: 3%;" action="tela_upload_lista.php" method="POST" enctype="multipart/form-data">
+            <div class="input-group mb-3">
+                <div class="custom-file">
+                    <input type="file" name="lista" class="custom-file-input" id="inputGroupFile02">
+                    <label class="custom-file-label" for="inputGroupFile02">Escolha o arquivo da lista</label>
+                </div>
+                <div class="input-group-append">
+                    <input type="submit" value="Fazer Upload" class="input-group-text">
+                </div>
+            </div>
         </form>
-	</div>
+    </div>
 </div>
 
-
-<?php require_once $abs_us_root.$us_url_root.'users/includes/html_footer.php'; ?>
+<?php require_once $abs_us_root . $us_url_root . 'users/includes/html_footer.php'; ?>
